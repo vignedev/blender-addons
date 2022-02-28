e3761a43084898ad43f0a099b2070dfe5115993a
vignedev <vignedev@gmail.com>
2022-02-28 10:18:43 +0100

    add notice to blender-gl

--- blender-gl.py
+++ blender-gl.py
@@ -25,8 +25,22 @@ class GLRendererInfo(bpy.types.Panel):
     vendor = bgl.glGetString(bgl.GL_VENDOR)
     version = bgl.glGetString(bgl.GL_VERSION)
     
+    workaround_path = '/sys/class/drm/card0/engine/rcs0/preempt_timeout_ms'
+    last_time_check = None
+
+    def get_timeout_ms(self):
+        with open(self.workaround_path) as file:
+            self.last_time_check = int(file.read())
+
     def draw(self, context):
         layout = self.layout
+
+        if 'Mesa Intel(R) UHD Graphics 620' in self.renderer:
+            if self.last_time_check is None:
+                self.get_timeout_ms()
+            if self.last_time_check <= 640:
+                box = layout.box()
+                box.label(text=f'preempt_timeout_ms seems too low! ({self.last_time_check}ms)', icon='ERROR')
 
         row = layout.row(align=True)
         row.label(text="Renderer: " + self.renderer)
