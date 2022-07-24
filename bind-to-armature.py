4155830d030f1b4359758f2842a2ddc7edcf57e2
vignedev <vignedev@gmail.com>
2022-07-24 23:42:45 +0200

    update bind-to-armature.py script for undo and QoL

--- bind-to-armature.py
+++ bind-to-armature.py
@@ -12,13 +12,17 @@ bl_info = {
 
 import bpy
 
-def main(context):
+def main(context, mapping, rest_pose):
     active_object = context.active_object
     objects = context.selected_objects
 
     armature = active_object.find_armature()
     if armature == None:
         raise Exception('No armature found on active_object')
+
+    org_pose_position = armature.data.pose_position
+    if rest_pose:
+        armature.data.pose_position = 'REST'
 
     context.view_layer.objects.active = armature
 
@@ -31,10 +35,9 @@ def main(context):
         context.view_layer.objects.active = armature
         bpy.ops.object.parent_set(type='ARMATURE_NAME')
 
-
         mod_transfer = cur_object.modifiers.new(name='tmp_bind-to-armature', type='DATA_TRANSFER')
         mod_transfer.data_types_verts = { 'VGROUP_WEIGHTS' }
-        mod_transfer.vert_mapping = 'NEAREST'
+        mod_transfer.vert_mapping = mapping
         mod_transfer.object = active_object
 
         bpy.context.view_layer.objects.active = cur_object
@@ -43,20 +46,41 @@ def main(context):
         bpy.ops.object.modifier_apply(modifier='tmp_bind-to-armature')
         bpy.ops.object.vertex_group_clean(group_select_mode='ALL', limit=0, keep_single=False)
 
+    if rest_pose:        
+        armature.data.pose_position = org_pose_position
+    context.view_layer.objects.active = active_object
+
 
 
 class BindToArmatureOperator(bpy.types.Operator):
     """Bind a mesh to an already rigged mesh using Data Transfer method."""
     bl_idname = "object.bind_to_armature"
     bl_label = "Bind to Armature"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    vertex_mapping: bpy.props.EnumProperty(name='Vertex Mapping', items=[
+        ('TOPOLOGY', 'Topology', '', 1),
+        ('NEAREST', 'Nearest Vertex', '', 2),
+        ('EDGE_NEAREST', 'Nearest Edge Vertex', '', 3),
+        ('EDGEINTERP_NEAREST', 'Nearest Edge Interpolated', '', 4),
+        ('POLY_NEAREST', 'Nearest Face Vertex', '', 5),
+        ('POLYINTERP_NEAREST', 'Nearest Face Interpolated', '', 6),
+        ('POLYINTERP_VNORPROJ', 'Projected Face Interpolated', '', 7)
+    ], default='NEAREST')
+
+    rest_pose: bpy.props.BoolProperty(name='Use Rest Position', default=True)
 
     @classmethod
     def poll(cls, context):
-        return context.active_object is not None
+        return context.active_object != None
 
     def execute(self, context):
-        main(context)
+        main(context, str(self.vertex_mapping), bool(self.rest_pose))
         return {'FINISHED'}
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
 
 
 def register():
